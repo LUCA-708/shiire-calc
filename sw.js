cf05fa2d8a1d936431ab52f000e3f82e144cd75a
yamam <yamam@local>
2026-09-17 13:55:00 +0900

    更新: index.html / sw.js（shiire-v8）

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
        版番号を上げ忘れたことは機械では分からない＝人が気をつける）
 */
 
-var CACHE = "shiire-v7";
+var CACHE = "shiire-v8";
 
 // 控える物の一覧。"./" は入口（ホーム画面のアイコンが開くアドレス）＝中身は index.html
 var FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png"];
